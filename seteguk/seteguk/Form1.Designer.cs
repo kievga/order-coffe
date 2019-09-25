678bc57d071e09d95fa61ca9d3adea3fef280335
Bella Dwi Rahmatulia <54664750+belladwir@users.noreply.github.com>
2019-09-25 21:53:07 +0700

    Add files via upload

---
+++ seteguk/seteguk/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace seteguk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ColumnHeader columnHeader1;
+            this.listView_order = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.orderdetail = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.text_nama = new System.Windows.Forms.TextBox();
+            this.text_total = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.text_id = new System.Windows.Forms.TextBox();
+            columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "id";
+            columnHeader1.Width = 37;
+            // 
+            // listView_order
+            // 
+            this.listView_order.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView_order.HideSelection = false;
+            this.listView_order.Location = new System.Drawing.Point(12, 110);
+            this.listView_order.Name = "listView_order";
+            this.listView_order.Size = new System.Drawing.Size(229, 328);
+            this.listView_order.TabIndex = 0;
+            this.listView_order.UseCompatibleStateImageBehavior = false;
+            this.listView_order.View = System.Windows.Forms.View.Details;
+            this.listView_order.SelectedIndexChanged += new System.EventHandler(this.ListView_order_SelectedIndexChanged);
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nama";
+            this.columnHeader2.Width = 94;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Status";
+            this.columnHeader3.Width = 88;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarTitleBackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.dateTimePicker1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dateTimePicker1.Enabled = false;
+            this.dateTimePicker1.Location = new System.Drawing.Point(15, 66);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(226, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(241, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 45);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Order List";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(575, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "selesai";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(246, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Customer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(246, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Order Detail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(246, 342);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Total Harga";
+            // 
+            // orderdetail
+            // 
+            this.orderdetail.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5});
+            this.orderdetail.HideSelection = false;
+            this.orderdetail.Location = new System.Drawing.Point(315, 216);
+            this.orderdetail.Name = "orderdetail";
+            this.orderdetail.Size = new System.Drawing.Size(334, 117);
+            this.orderdetail.TabIndex = 10;
+            this.orderdetail.UseCompatibleStateImageBehavior = false;
+            this.orderdetail.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Menu";
+            this.columnHeader4.Width = 168;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Jumlah";
+            this.columnHeader5.Width = 150;
+            // 
+            // text_nama
+            // 
+            this.text_nama.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.text_nama.Location = new System.Drawing.Point(316, 190);
+            this.text_nama.Name = "text_nama";
+            this.text_nama.ReadOnly = true;
+            this.text_nama.Size = new System.Drawing.Size(334, 20);
+            this.text_nama.TabIndex = 11;
+            // 
+            // text_total
+            // 
+            this.text_total.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.text_total.Location = new System.Drawing.Point(316, 339);
+            this.text_total.Name = "text_total";
+            this.text_total.ReadOnly = true;
+            this.text_total.Size = new System.Drawing.Size(334, 20);
+            this.text_total.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(426, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 25);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Order Detail";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(246, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "ID Pemesanan : ";
+            // 
+            // text_id
+            // 
+            this.text_id.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.text_id.Location = new System.Drawing.Point(339, 164);
+            this.text_id.Name = "text_id";
+            this.text_id.ReadOnly = true;
+            this.text_id.Size = new System.Drawing.Size(100, 20);
+            this.text_id.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 450);
+            this.Controls.Add(this.text_id);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.text_total);
+            this.Controls.Add(this.text_nama);
+            this.Controls.Add(this.orderdetail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.listView_order);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView_order;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView orderdetail;
+        private System.Windows.Forms.TextBox text_nama;
+        private System.Windows.Forms.TextBox text_total;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox text_id;
+    }
+}
+
---
+++ seteguk/seteguk/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace seteguk
+{
+    public partial class Form1 : Form
+    {
+        private static string connectionString = "server=localhost;port=3306;username=root;password=;database=seteguk;";
+        private MySqlConnection databaseConn = new MySqlConnection(connectionString);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void getPemesanan()
+        {
+            string query = "SELECT * FROM stgk_pemesanan INNER JOIN stgk_menu ON stgk_pemesanan.id_menu = stgk_menu.id_menu";
+            try
+            {
+                //koneksi dibuka
+                databaseConn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, databaseConn);
+                cmd.CommandTimeout = 60;
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        ListViewItem listpemesanan = new ListViewItem(reader["id_pemesanan"].ToString());
+                        listpemesanan.SubItems.Add(reader["nama_pembeli"].ToString());
+                        listpemesanan.SubItems.Add(reader["status"].ToString());
+                        listView_order.Items.Add(listpemesanan);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Tidak Ada data");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //tutup koneksi
+                databaseConn.Close();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            listView_order.Items.Clear();
+            string query= "UPDATE stgk_pemesanan SET status = 'selesai' where id_pemesanan = @id";
+            try
+            {
+                //Open koneksi database
+                databaseConn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, databaseConn);
+                cmd.CommandTimeout = 60;
+                cmd.Parameters.AddWithValue("@id", text_id.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Pemesanan telah selesai");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                //tutup databasenya
+                databaseConn.Close();
+                getPemesanan();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getPemesanan();
+        }
+
+        ListViewItem order;
+        private void ListView_order_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM stgk_pemesanan INNER JOIN stgk_menu ON stgk_pemesanan.id_menu = stgk_menu.id_menu WHERE stgk_pemesanan.id_pemesanan = @id_pemesanan;";
+
+            if (listView_order.SelectedItems.Count > 0)
+            {
+                order = listView_order.SelectedItems[0];
+                text_id.Text = order.SubItems[0].Text;
+
+                //koneksi dibuka
+                    databaseConn.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, databaseConn);
+                    cmd.CommandTimeout = 60;
+                    cmd.Parameters.AddWithValue("@id_pemesanan", text_id.Text);
+                    cmd.ExecuteNonQuery();
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            ListViewItem orderlist = new ListViewItem(reader["nama_menu"].ToString());
+                            orderlist.SubItems.Add(reader["jumlah"].ToString());
+                            orderdetail.Items.Add(orderlist);
+                            text_nama.Text = reader["nama_pembeli"].ToString();
+                            text_total.Text = reader["total_harga"].ToString();
+                        }
+                        reader.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Tidak Order Detail");
+                    }
+            }
+            else
+            {
+                orderdetail.Items.Clear();
+                text_id.Text = "";
+                text_nama.Text = "";
+                text_total.Text = "";
+            }
+            databaseConn.Close();
+        }
+    }
+}
